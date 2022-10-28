c805f14662fc2cabf058daabb7080a546351d1cc
EstomJr <estonjr2@hotmail.com>
2022-10-28 18:14:06 -0300

    Create project

---
+++ Desafio_k-lab/src/main/java/com/desafioklab/Desafio_klab/DesafioKLabApplication.java
@@ -0,0 +1,13 @@
+package com.desafioklab.Desafio_klab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioKLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioKLabApplication.class, args);
+	}
+
+}
---
+++ Desafio_k-lab/src/test/java/com/desafioklab/Desafio_klab/DesafioKLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafioklab.Desafio_klab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioKLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
