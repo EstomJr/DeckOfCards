0c681c74211106b49c6095e09e48573e0372f4bb
EstomJr <estonjr2@hotmail.com>
2022-11-03 15:01:47 -0300

    Creation of FeignClient with api deckofcards

---
+++ Desafio_k-lab/src/main/java/com/desafioklab/Desafio_klab/client/DeckOfCardsApi.java
@@ -0,0 +1,15 @@
+package com.desafioklab.Desafio_klab.client;
+
+import deck.entities.Root;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@FeignClient(value = "DeckOfCardsapi", url = "https://deckofcardsapi.com/")
+public interface DeckOfCardsApi {
+
+    @RequestMapping(method = RequestMethod.GET, value = "api/deck/{deck_id}/draw/?count={count}"    )
+    Root getDeck(@PathVariable("deck_id") String deckId, @PathVariable("count") int count);
+
+}
