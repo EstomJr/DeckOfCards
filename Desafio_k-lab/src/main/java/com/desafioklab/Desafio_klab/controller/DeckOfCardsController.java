ae82479c7427ebd2c65d788d8a73b79c272ef5c9
EstomJr <estonjr2@hotmail.com>
2022-11-03 16:00:08 -0300

    Update on controller for return winner and cards in json

--- Desafio_k-lab/src/main/java/com/desafioklab/Desafio_klab/controller/DeckOfCardsController.java
+++ Desafio_k-lab/src/main/java/com/desafioklab/Desafio_klab/controller/DeckOfCardsController.java
@@ -1,7 +1,9 @@
 package com.desafioklab.Desafio_klab.controller;
 
+import com.desafioklab.Desafio_klab.model.response.Jogo;
 import com.desafioklab.Desafio_klab.service.DeckOfCardsServices;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,8 +15,8 @@ public class DeckOfCardsController {
     private DeckOfCardsServices deckOfCardsServices;
 
     @RequestMapping(value = "/jogo", method = RequestMethod.GET)
-    public String jogar (){
-        deckOfCardsServices.jogarBaralhoComQuatroJogadores();
-        return "";
+    public ResponseEntity<Jogo> jogar (){
+        Jogo jogo = deckOfCardsServices.jogarBaralhoComQuatroJogadores();
+        return ResponseEntity.ok(jogo);
     }
 }
