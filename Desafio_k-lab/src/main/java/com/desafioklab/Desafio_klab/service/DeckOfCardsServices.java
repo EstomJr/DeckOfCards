5f0e3aea13fc847bf8c187abe773eb698bb477a7
EstomJr <estonjr2@hotmail.com>
2022-11-03 15:53:00 -0300

    Update on logic, now return winner in the response

--- Desafio_k-lab/src/main/java/com/desafioklab/Desafio_klab/service/DeckOfCardsServices.java
+++ Desafio_k-lab/src/main/java/com/desafioklab/Desafio_klab/service/DeckOfCardsServices.java
@@ -1,12 +1,16 @@
 package com.desafioklab.Desafio_klab.service;
 
+
 import com.desafioklab.Desafio_klab.client.DeckOfCardsApi;
 import com.desafioklab.Desafio_klab.enums.Cards;
 import com.desafioklab.Desafio_klab.model.response.Card;
-import deck.entities.Root;
+import com.desafioklab.Desafio_klab.model.response.Jogador;
+import com.desafioklab.Desafio_klab.model.response.Jogo;
+import com.desafioklab.Desafio_klab.model.response.Root;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -16,30 +20,38 @@ public class DeckOfCardsServices {
     @Autowired
     DeckOfCardsApi deckOfCardsApi;
 
-    public void jogarBaralhoComQuatroJogadores(){
+    public Jogo jogarBaralhoComQuatroJogadores(){
         String deckId = "new";
         int maiorNumeroDeCartas =  -1;
         int jogadorVencedor = -1;
-        List<String> ecardsToNumber = Arrays.asList(Cards.ACE.name(), Cards.JACK.name(), Cards.KING.name(), Cards.QUEEN.name());
-        for (int i = 0; i <4; i++){
-           Root cards = deckOfCardsApi.getDeck(deckId,4);
-           if(cards.getSuccess()){
-               deckId = cards.getDeck_id();
-               int soma = getSomaCartas(ecardsToNumber, i, cards);
-               if(soma > maiorNumeroDeCartas){
-                   jogadorVencedor = i;
-               }
-           }
+        List<String> ecardsToNumber = Arrays.stream(Cards.values()).map(c -> c.name()).toList();
+        List<Jogador> jogadors = new ArrayList<>();
+        Jogo jogo = new Jogo();
+        for (int i = 1; i <=4; i++){
+            Root cards = deckOfCardsApi.getDeck(deckId,4);
+            if(cards.getSuccess()){
+                deckId = cards.getDeckId();
+                int soma = getSomaCartas(ecardsToNumber, i, cards);
+                Jogador jogador = new Jogador(i, soma,cards.getCards());
+                if(soma > maiorNumeroDeCartas){
+                    maiorNumeroDeCartas = soma;
+                    jogadorVencedor = i;
+                }
+                jogadors.add(jogador);
+            }
+
         }
-        System.out.println("Vencedor jogador " + jogadorVencedor);
+        jogo.setNumeroDoDeck(deckId);
+        jogo.setJogadorVencedor(String.valueOf(jogadorVencedor));
+        jogo.setJogadorList(jogadors);
+        jogo.setPontosJogadorVencedor(String.valueOf(maiorNumeroDeCartas));
+        return jogo;
 
     }
 
     private static int getSomaCartas(List<String> ecardsToNumber, int i, Root cards) {
         int soma = 0;
-        for (Card card:
-             cards.getCards()) {
-            System.out.println("Jogador " + i + " com carta " + card.getValue());
+        for (Card card: cards.getCards()) {
 
             if(ecardsToNumber.contains(card.getValue())){
                 soma = getValueOfCardsWithNames(soma, card);
@@ -51,8 +63,7 @@ public class DeckOfCardsServices {
     }
 
     private static int getValueOfCardsWithNames(int soma, Card card) {
-        for (Cards cardEnum:
-             Cards.values()) {
+        for (Cards cardEnum: Cards.values()) {
             if(cardEnum.name().equals(card.getValue())){
                 soma += cardEnum.getValor();
             }
