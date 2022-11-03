e3a8891f9c2f42157a089da94988f997d0babd5c
EstomJr <estonjr2@hotmail.com>
2022-11-03 15:14:50 -0300

    Update model/response with the class Jogo

---
+++ Desafio_k-lab/src/main/java/com/desafioklab/Desafio_klab/model/response/Jogo.java
@@ -0,0 +1,26 @@
+package com.desafioklab.Desafio_klab.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Jogo {
+
+    @JsonProperty("jogador_vencedor")
+    private String jogadorVencedor;
+
+    @JsonProperty("pontos_jogador_vencedor")
+    private String pontosJogadorVencedor;
+
+    @JsonProperty("deck_id")
+    private String numeroDoDeck;
+
+    @JsonProperty("jogadores")
+    private List<Jogador> jogadorList;
+}
